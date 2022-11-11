e8faad47a104d160881fcf687721ebab78648071
baktiyar <baktiyar.tynchyev@gmail.com>
2022-11-11 22:27:31 +0600

    added new sout which is not on baha2

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Human human = new Human("Argen");
+        System.out.println(human.getName());
+        System.out.println("i changed new thing that it doesnt have on baha2");
+
+    }
+}
